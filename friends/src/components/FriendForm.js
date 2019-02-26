01e4e4d313cec96a03747a99815e5e83300da9d4
ills5771 <ilyayelizarov1@gmail.com>
2019-02-26 15:59:33 -0800

    passed props to FriendForm.js from App

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,21 +1,11 @@
 import React from "react";
 
-function FriendForm() {
+function FriendForm(props) {
   return (
-    <form>
-      <input
-        type="text"
-        placeholder="Name"
-        name="name"
-        value={this.state.name}
-      />
-      <input type="text" placeholder="Age" name="age" value={this.state.age} />
-      <input
-        type="text"
-        placeholder="Email"
-        name="email"
-        value={this.state.email}
-      />
+    <form onSubmit={props.addFriend}>
+      <input type="text" placeholder="Name" name="name" value={props.name} />
+      <input type="text" placeholder="Age" name="age" value={props.age} />
+      <input type="text" placeholder="Email" name="email" value={props.email} />
 
       <button>Add</button>
     </form>
